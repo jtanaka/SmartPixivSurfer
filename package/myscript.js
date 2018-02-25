d6cc7c2889c2d94f04bd69615b71fc2ae812dfc8
Jumpei Tanaka <jum.tanaka@gmail.com>
2018-02-25 22:39:53 +0900

    UI の変更に対応

--- package/myscript.js
+++ package/myscript.js
@@ -4,12 +4,13 @@ $(function(){
 	const info_selector = ".work-info";
 	const tag_selector = ".work-tags";
 	const sns_selector = ".reaction-container";
-	const thumbnail_selector = "div.ui-scroll-view.profile-module";
-	const thumbnail_wrapper_selector = "div.ui-scroll-view.profile-module > div";
-	const thumbnail_profile_selector = "div.ui-scroll-view.profile-module > div > div";
+	const thumbnail_title_selector = ".sQnmXM9";
+	const thumbnail_selector = ".ui-scroll-view";
+	const showcase_selector = ".showcase-container";
+	const link_selector =".other-links-container";
 
 	$(info_selector).before($(illust_selector));
-	$(illust_selector).after($(thumbnail_selector));
+	$(illust_selector).after($(link_selector));
 
 	$(".layout-column-2").fadeIn(500);
 });
